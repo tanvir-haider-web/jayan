e277957bf4c3e5e3b5b519c94ce4406783cb8473
tanvir-haider-web <haidert@sheridancollege.ca>
2019-10-07 21:03:57 -0400

    2nd time added codes for new repo called GitHub

--- src/github/GitHub.java
+++ src/github/GitHub.java
@@ -13,7 +13,7 @@ public class GitHub {
 
    
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Canada");
         
         
         
